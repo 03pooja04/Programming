a47eb6c7d9ba8064254c1652fdf78b6f47df16c1
03pooja04 <137779586+03pooja04@users.noreply.github.com>
2024-06-02 13:54:22 +0530

    Add files via upload

---
+++ Program1.java
@@ -0,0 +1,34 @@
+class Q1{
+public static void main(String [] args)
+{
+	int line=5;
+        int star=1;
+	int space=8;
+
+for(int i=0;i<line;i++)
+{
+int a=i;
+int b=1;
+	for(int j=1;j<star;j++)
+	{
+	System.out.print(b);
+	b++;
+        }
+		for(int k=0;k<space;k++)
+		{
+		System.out.print(" ");
+		}
+                for(int i=0;i<star;i++)
+                {
+                System.out.print(a);
+                a++;
+                }
+		System.out.println();
+		}	
+		star++;
+		space-=2;
+		}
+		}
+		}
+                }
+		
