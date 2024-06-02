b4c71d67fccb654f70fd03305b6c9568ceb23aad
03pooja04 <137779586+03pooja04@users.noreply.github.com>
2024-06-02 13:58:33 +0530

    Add files via upload

---
+++ Program3.java.java
@@ -0,0 +1,12 @@
+class pattern1{
+	public static void main (String[] args)
+	{
+		for( int j=0;j<2;j++){
+			for(i=0;i<4;i++){
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+
+	}
+}
